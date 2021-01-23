7751006d2a1c44f905c658687bdbe749a2c4476b
NLD37BYs <line51634@gmail.com>
2021-01-23 20:14:47 +0800

    add hello5

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     println! {"Hello World!"};
     hello2();
     hello3();
-    hello4()
+    hello4();
+    hello5()
 }
 
 fn hello2() {
@@ -17,3 +18,6 @@ fn hello4() {
     println!("Hello Hi Hello World!");
 }
 
+fn hello5() {
+    println!("Hellll llllll!");
+}
